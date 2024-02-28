cd46de8dcc8129d5dfa5f49e26e8977d9775bb12
unknown <timafe771>
2024-02-29 03:42:21 +0900

    feat: develop member schema model

--- src/routerAdmin.ts
+++ src/routerAdmin.ts
@@ -2,4 +2,8 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
+routerAdmin.get("/", restaurantController.getHome);
+routerAdmin.get("/login", restaurantController.getHome);
+routerAdmin.get("/singup", restaurantController.getHome);
+
 export default routerAdmin;
