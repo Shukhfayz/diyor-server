bd8d6d12cedf2a8ed2c4066f5c66e8fba014077c
unknown <timafe771>
2024-02-29 15:35:04 +0900

    feat: develop member schema model

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,24 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+
+// H task
+
+/* 
+shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul qilib,
+ faqat positive qiymatlarni olib string holatda return qilsin
+MASALAN: getPositive([1, -4, 2]) return qiladi "12"
+*/
+function getPositive(arr: number[]): string {
+  let natija: string = "";
+
+  for (let i: number = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      natija += arr[i].toString();
+    }
+  }
+
+  return natija;
+}
+
+let array: number[] = [1, -4, 2];
+let resultString: string = getPositive(array);
+console.log(resultString);
