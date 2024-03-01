13d52915b6e9aace8a56289b7718d9d23fd04f9c
unknown <timafe771>
2024-03-01 19:48:28 +0900

    feat: create project standarts

--- src/train.ts
+++ src/train.ts
@@ -7,18 +7,29 @@ shunday function tuzing, u integerlardan iborat arrayni argument sifatida qabul 
  faqat positive qiymatlarni olib string holatda return qilsin
 MASALAN: getPositive([1, -4, 2]) return qiladi "12"
 */
-function getPositive(arr: number[]): string {
-  let natija: string = "";
+// function getPositive(arr: number[]): string {
+//   let natija: string = "";
 
-  for (let i: number = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      natija += arr[i].toString();
-    }
-  }
+//   for (let i: number = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       natija += arr[i].toString();
+//     }
+//   }
 
-  return natija;
-}
+//   return natija;
+// }
 
-let array: number[] = [1, -4, 2];
-let resultString: string = getPositive(array);
-console.log(resultString);
+// let array: number[] = [1, -4, 2];
+// let resultString: string = getPositive(array);
+// console.log(resultString);
+
+/* Project Standart:
+-- Logging standarts
+-- Naming standarts
+    function, method, variable => CAMEL     goHome
+    class => PASCAL                         MemberService
+    folder => KEBAB
+    css => SNAKE                            button_style
+
+
+*/
