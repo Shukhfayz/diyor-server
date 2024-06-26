a72a5476fc6a10b11085cac5ae619efa1c984014
timafe771 <timafe771@gmail.com>
2024-06-26 12:45:59 +0900

    feat: commit meassage

--- src/train.ts
+++ src/train.ts
@@ -1,42 +1,4 @@
-// console.log("Hello World!");
 
-/* Project Standart:
--- Logging standarts
--- Naming standarts
-    function, method, variable => CAMEL     goHome
-    class => PASCAL                         MemberService
-    folder, file=> KEBAB
-    css => SNAKE                            button_style
-
-
-*/
-
-/** Request:
- Traditional Api
- Rest Api
- GraphQL Api
- ......
- */
-
-/*Frontend Deevelopment
-  Traditional FD => BSSR (Admin) => EJS
-  Modern FD      => SPA => REACT
-
-  */
-
-/**Cookies:
- request join 
- self destroy
- */
-
-/** Validation 
-  Frontend validation
-  Backend validation 
-  Database validation
-  
-  */
-
-// Task: ZI
 function reduceNestedArray(arr: (number | any)[]): number {
   let total: number = 0;
 
